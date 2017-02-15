5a40a80f8d7c436e3fe2acb544bd4f9ba55d82ae
Jonathan Lloyd <jonathanlloyd@Jonathans-MacBook-Pro.local>
2017-02-15 14:06:46 -0800

    message

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by jonathanlloyd on 2/15/17.
+ */
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return "";
+    }
+}
---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by jonathanlloyd on 2/15/17.
+ */
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return "";
+    }
+}
